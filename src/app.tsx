636a5a5e4bd4c50a8cf8781fd5992b0dde15c7a5
daiki_beppu (別府 大貴) <140964131+daiki-beppu@users.noreply.github.com>
2026-01-17 18:06:24 +0900

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from "@react-three/fiber";
+
+export const App = () => (
+  <div className="w-dvw h-dvh overflow-hidden">
+    <Canvas>
+      <mesh>
+        <sphereGeometry />
+        <meshNormalMaterial wireframe />
+      </mesh>
+    </Canvas>
+  </div>
+);
